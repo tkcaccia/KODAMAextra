77547ccbc1e40359b63d3ab804f7e38a9c090f74
tkcaccia <tkcaccia@gmail.com>
2023-11-07 13:48:08 +0200

    Update main.R

--- R/main.R
+++ R/main.R
@@ -366,3 +366,253 @@ spatial.knn = 10, splitting = 50, clust_contrain = FALSE, n.cores=1)
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+                              
+KODAMA.matrix.parallel =
+function (data,                       # Dataset
+          spatial = NULL,             # In spatial are conteined the spatial coordinates of each entries
+          M = 100, Tcycle = 20, 
+          FUN = c("PLS","PK","KNN"), 
+          f.par.knn = 5, f.par.pls = 5,
+          W = NULL, 
+          constrain = NULL, fix = NULL, epsilon = 0.05, landmarks = 10000,  
+          splitting = 50, spatial.resolution = 0.3, ncore = 1) 
+{
+  neighbors = min(c(landmarks, nrow(data)/3)) + 1
+  if (sum(is.na(data)) > 0) {
+    stop("Missing values are present")
+  } 
+  data = as.matrix(data)
+  nsample = nrow(data)
+  nvariable = ncol(data)
+  nsample_spatial= nrow(spatial)
+  
+  if (is.null(spatial)) {
+    spatial_flag = FALSE
+  }  else {
+    spatial_flag = TRUE
+  }
+  if (is.null(fix)) 
+    fix = rep(FALSE, nsample)
+  if (is.null(constrain)) 
+    constrain = 1:nsample
+  shake = FALSE
+  
+  if(nsample<=landmarks){
+    landmarks=ceiling(nsample*0.75)
+    simm_dissimilarity_matrix=TRUE
+  } else{
+    simm_dissimilarity_matrix=FALSE
+  }
+
+  nspatialclusters=round(landmarks*spatial.resolution)
+  
+  QC=quality_control(data_row = nsample,
+                     data_col = nvariable,
+                     spatial_row = nsample_spatial,
+                     FUN = FUN,
+                     data = data,
+                     f.par.knn = f.par.knn,
+                     f.par.pls = f.par.pls)
+  matchFUN=QC$matchFUN
+
+  f.par.pls=QC$f.par.pls
+
+
+
+  res = matrix(nrow = M, ncol = nsample)
+
+
+  vect_acc = matrix(NA, nrow = M, ncol = Tcycle)
+  accu = NULL
+
+#  pb <- txtProgressBar(min = 1, max = M, style = 1)
+#  for (k in 1:M) {
+#    setTxtProgressBar(pb, k)
+
+  my.cluster <- parallel::makeCluster(
+    n.cores, 
+    type = "PSOCK"
+  )
+  print(my.cluster)
+  doParallel::registerDoParallel(cl = my.cluster)
+  foreach::getDoParRegistered()
+  foreach::getDoParWorkers()
+  res_parallel=foreach(k=1:M) %dopar%   {
+    library("KODAMA")
+  
+
+    
+
+  
+    # The landmarks samples are chosen in a way to cover all different profile
+    # The data are divided in a number of clusters equal to the number of landmarks
+    # A landpoint is chosen randomly from each cluster
+
+    landpoints=NULL
+    clust = as.numeric(kmeans(data, landmarks)$cluster)
+    for (ii in 1:landmarks) {
+      www = which(clust == ii)
+      lwww=length(www)
+      landpoints = c(landpoints,www[sample.int(lwww, 1, FALSE, NULL)])
+    }
+
+    # Variables are splitted in two where 
+    # X variables are the variables for cross-validation accuracy maximizzation
+    # T variables are the variable for the projections
+    
+    Tdata = data[-landpoints, , drop = FALSE]
+    Xdata = data[landpoints, , drop = FALSE]
+    Tfix = fix[-landpoints]
+    Xfix = fix[landpoints]
+    whF = which(!Xfix)
+    whT = which(Xfix)
+    Tconstrain = as.numeric(as.factor(constrain[-landpoints]))
+    Xconstrain = as.numeric(as.factor(constrain[landpoints]))
+    Xspatial = spatial[landpoints, , drop = FALSE]
+    Tspatial = spatial[-landpoints, , drop = FALSE]
+
+    
+ 
+
+    if (spatial_flag) {
+      spatialclusters=as.numeric(kmeans(Xspatial, nspatialclusters)$cluster)
+      tab = apply(table(spatialclusters, Xconstrain), 2,which.max)
+      Xconstrain = as.numeric(as.factor(tab[as.character(Xconstrain)]))  
+    }
+  
+    if (landmarks<200) {
+      XW = Xconstrain
+    } else {
+      clust = as.numeric(kmeans(Xdata, splitting)$cluster)
+      tab = apply(table(clust, Xconstrain), 2, which.max)
+      XW = as.numeric(as.factor(tab[as.character(Xconstrain)]))
+    }
+
+    if(!is.null(W)){
+      SV_startingvector = W[landpoints][ssa]
+      unw = unique(SV_startingvector)
+      unw = unw[-which(is.na(unw))]
+      ghg = is.na(SV_startingvector)
+      SV_startingvector[ghg] = as.numeric(as.factor(SV_startingvector[ghg])) + length(unw)
+      tab = apply(table(SV_startingvector,Xconstrain), 2,  which.max)
+      XW = as.numeric(as.factor(tab[as.character(Xconstrain)]))
+    }
+   
+    
+    clbest = XW
+    options(warn = -1)
+    yatta = 0
+    attr(yatta, "class") = "try-error"
+    while (!is.null(attr(yatta, "class"))) {
+      yatta = try(core_cpp(Xdata, Tdata, clbest, Tcycle, FUN, 
+                           f.par.knn,f.par.pls,
+                           Xconstrain, Xfix, shake, Xspatial, 
+                           Tspatial), silent = FALSE)
+
+    }
+    options(warn = 0)
+    res_k=NULL
+    if (is.list(yatta)) {
+      clbest = as.vector(yatta$clbest)
+      accu = yatta$accbest
+      yatta$vect_acc = as.vector(yatta$vect_acc)
+      yatta$vect_acc[yatta$vect_acc == -1] = NA
+      vect_acc[k, ] = yatta$vect_acc
+      
+      yatta$vect_proj = as.vector(yatta$vect_proj)
+      yatta$vect_proj[Tfix] = W[-landpoints][Tfix]
+
+      res_k[landpoints] = clbest
+      res_k[-landpoints] = yatta$vect_proj
+
+
+
+      
+    }
+ # }
+#  close(pb)
+
+    list(res_k=res_k)
+
+  }
+  parallel::stopCluster(cl = my.cluster)
+  
+  for(k in 1:M){
+    res[k,] = res_parallel[[k]]$res_k
+  }
+#  close(pb)
+
+  
+
+    
+  dissimilarity=NULL
+  ma=NULL
+  if(simm_dissimilarity_matrix){
+    ma = matrix(0, ncol = nsample, nrow = nsample)
+    for(k in 1:M){
+      uni = unique(res[k,])
+      nun = length(uni)
+      res_k=res[k,]
+      for (ii in 1:nun) 
+        ma[res[k,] == uni[ii], res_k ==  uni[ii]] = ma[res_k == uni[ii], res_k == uni[ii]] + 1
+    }
+    ma = ma/M
+    Edist = as.matrix(dist(data))
+    ma[ma < epsilon] = 0
+
+#  Entropy calculation
+#    y = ma
+#    diag(y) = NA
+#    yy = as.numeric(y)
+#    yy = yy[!is.na(yy)]
+#    yy = yy/sum(yy)
+#    H = -sum(ifelse(yy > 0, yy * log(yy), 0))
+    
+    mam = (1/ma) * Edist
+    mam[is.na(mam)] <- .Machine$double.xmax
+    mam[is.infinite(mam) & mam > 0] <- .Machine$double.xmax
+    mam = floyd(mam)
+    mam[mam == .Machine$double.xmax] <- NA
+    prox = Edist/mam
+    diag(prox) = 1
+    prox[is.na(prox)] = 0
+    maxvalue = max(mam, na.rm = TRUE)
+    mam[is.na(mam)] = maxvalue
+
+    dissimilarity = mam
+  }
+
+  knn_Armadillo = knn_Armadillo(data, data, neighbors + 1)
+  knn_Armadillo$distances = knn_Armadillo$distances[, -1]
+  knn_Armadillo$nn_index = knn_Armadillo$nn_index[, -1]
+  for (i_tsne in 1:nrow(data)) {
+    for (j_tsne in 1:neighbors) {
+      kod_tsne = mean(res[, i_tsne] == res[, knn_Armadillo$nn_index[i_tsne, j_tsne]], na.rm = TRUE)
+      knn_Armadillo$distances[i_tsne, j_tsne] = knn_Armadillo$distances[i_tsne,  j_tsne]/kod_tsne
+    }
+    oo_tsne = order(knn_Armadillo$distance[i_tsne, ])
+    knn_Armadillo$distances[i_tsne, ] = knn_Armadillo$distances[i_tsne, oo_tsne]
+    knn_Armadillo$nn_index[i_tsne, ] = knn_Armadillo$nn_index[i_tsne, oo_tsne]
+  }
+
+  knn_Armadillo$neighbors = neighbors
+  return(list(dissimilarity = dissimilarity, acc = accu, proximity = ma, 
+              v = vect_acc, res = res, 
+              landpoints = landpoints, knn_Armadillo = knn_Armadillo, 
+              data = data))
+}
+
+
